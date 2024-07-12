024484348bdc812004c3c96219eb5bf87c8e5f86
Steely <vinicius.adriel@proton.me>
2024-07-12 08:40:44 -0300

    committed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+//const SEGUNDOS_MINUTOS: u16 = 60;
+//const SEGUNDOS_HORAS: u16 = SEGUNDOS_MINUTOS * 60;
+
 fn main() {
-    println!("Hello, world!");
+    let ch: &str = "World";
+    println!("Hello, {ch}!");
+
+    //let text = "Hello World!";
+
+    //let num = [1, 2, 3];
+    //println!("{:?}", &num[..]);
 }

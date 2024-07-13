d0647c2e1b7c2a7d72dd0c8b22efc57cc13df7f4
Steely <vinicius.adriel@proton.me>
2024-07-13 17:35:19 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
-//const SEGUNDOS_MINUTOS: u16 = 60;
-//const SEGUNDOS_HORAS: u16 = SEGUNDOS_MINUTOS * 60;
-
+fn helloworld(a: &str) {
+    println!("{a}");
+}
 fn main() {
-    let ch: &str = "World";
-    println!("Hello, {ch}!");
-
-    //let text = "Hello World!";
-
-    //let num = [1, 2, 3];
-    //println!("{:?}", &num[..]);
+    helloworld("println!");
 }

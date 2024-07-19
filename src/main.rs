630ce732852886864514ba5975cf77afdbb8c298
Steely <82281833+SteelStoneWasTaken@users.noreply.github.com>
2024-07-19 19:13:37 -0300

    só sucesso

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 use std::{io, process::exit};
+extern crate rand;
+use rand::Rng;
 
 static  INVALID_OP: &str= "OPÇÃO INVÁLIDA.";
 static  ERR_COORD1: &str= "COORDENADA OCUPADA.";
-static  TURNO: &str = "X";
+static  mut TURNO: &'static str = "";
 
+
+fn random(){
+    let mut rng = rand::thread_rng();
+    let random: bool = rng.gen();
+    match random {
+        true => unsafe{TURNO = "X"},
+        false    => unsafe{TURNO = "O"}
+    }
+}
 
 fn main() {
     println!(
@@ -14,30 +25,84 @@ fn main() {
         \nDigite o nome ou o número  \
         \nda opção desejada.         \
         \n                           \
-        \n1 - Começar.               \
-        \n2 - Configurações. (temp.) \
-        \n3 - Sair.
+        \n[1] Começar                \
+        \n[2] Configurações          \
+        \n[3] Sair 
         "
     );
 
-    //PADRÃO ALEATÓRIO OU ESCOLHIDO PELO JOGADOR (menu: configurações)
+    if unsafe {TURNO} == ""{
+        random();
+    }
     
     loop {
-        let mut start_selection = String::new();
+        let mut selection = String::new();
         io::stdin()
-            .read_line(&mut start_selection)
+            .read_line(&mut selection)
             .expect("{INVALID_OP}");
         
-        let start_selection = start_selection.trim().to_uppercase();
+        let selection = selection.trim().to_uppercase();
         
-        match start_selection.as_str() {
-            "COMEÇAR" | "1" => jogo(TURNO),
-            "SAIR"    | "3" => exit(0),
+        match selection.as_str() {
+            "COMEÇAR"       | "1" => jogo(unsafe {TURNO}),
+            "CONFIGURAÇÕES" | "2" => config(),
+            "SAIR"          | "3" => exit(0),
             _ => println!("{INVALID_OP}")
         }
     }
 }
 
+
+
+fn config(){
+    println!("\
+        \n[1] Trocar jogador inicial (atual: {})\
+        \n[2] Voltar                                                                   
+        ", unsafe {TURNO});
+    loop {
+        let mut selection = String::new();
+        io::stdin()
+            .read_line(&mut selection)
+            .expect("{INVALID_OP}");
+        
+        let selection = selection.trim().to_uppercase();
+        
+        match selection.as_str() {
+            "TROCAR JOGADOR INICIAL"  | "1" => pjogador(),
+            "VOLTAR"                  | "2" => main(),
+            "SAIR"                          => exit(0),
+            _ => println!("{INVALID_OP}")
+        }
+    }
+}
+
+
+
+fn pjogador() {
+    println!("\
+        \n[1] Definir X como jogador inicial \
+        \n[2] Definir O como jogador inicial \
+        \n[3] Aleatório como jogador inicial\
+        \n[4] Voltar                                                                   
+        ");
+    loop {
+        let mut selection = String::new();
+        io::stdin()
+            .read_line(&mut selection)
+            .expect("{INVALID_OP}");
+        
+        let selection = selection.trim().to_uppercase();
+        
+        match selection.as_str() {
+            "DEFINIR X COMO JOGADOR INICIAL"  | "1" => {unsafe{TURNO = "X"}; main()},
+            "DEFINIR O COMO JOGADOR INICIAL"  | "2" => {unsafe{TURNO = "O"}; main()},
+            "ALEATÓRIO COMO JOGADOR INICIAL"  | "3" => {random(); main()},
+            "VOLTAR"                          | "4" => config(),
+            "SAIR"                          => exit(0),
+            _ => println!("{INVALID_OP}")
+        }
+    }
+}
 
 
 fn jogo(mut turno: &str) {
@@ -94,7 +159,7 @@ fn jogo(mut turno: &str) {
         \n+-----------+\
         \n|  Empate!  |\
         \n+-----------+
-        "); exit(0);
+        "); endgame();
 }
 
 
@@ -120,7 +185,32 @@ fn verificar(turno: &str, map: [&str; 9]) {
             \n|  C | {} | {} | {}  |\
             \n+-----------------+\
             \n|  Vitória de {turno}!  |\
-            \n+-----------------+
+            \n+-----------------+\
             ",map[0],map[1],map[2],map[3],map[4],map[5],map[6],map[7],map[8]);
-    exit(0);}
+    endgame();}
+}
+
+
+
+fn endgame() {
+    println!("\
+        \n[1] Jogar novamente \
+        \n[2] Menu Principal  \
+        \n[3] Sair
+        ");
+    loop {
+        let mut selection = String::new();
+        io::stdin()
+            .read_line(&mut selection)
+            .expect("{INVALID_OP}");
+        
+        let selection = selection.trim().to_uppercase();
+        
+        match selection.as_str() {
+            "JOGAR NOVAMENTE" | "1" => jogo(unsafe {TURNO}),
+            "MENU PRINCIPAL"  | "2" => main(),
+            "SAIR"            | "3" => exit(0),
+            _ => println!("{INVALID_OP}")
+        }
+    }
 }
